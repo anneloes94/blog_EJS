02a8dda749785bd11b439584d4560be6d08eb013
Anneloes <anneloes9405@hotmail.com>
2020-08-05 20:14:30 -0400

    added compose post file
    Signed-off-by: Anneloes <anneloes9405@hotmail.com>

--- app.js
+++ app.js
@@ -27,6 +27,15 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent});  
 })
 
+app.get("/compose", function(req, res){  
+  res.render("compose");  
+})
+
+app.get("/post", function(req, res){
+    
+  res.render("compose");  
+})
+
 
 
 
